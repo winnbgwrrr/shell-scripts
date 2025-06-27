6cc9da47379f873bbee2146d175393617616b375
Robert Winslow <winslow.robert@bls.gov>
2025-06-27 18:54:35 -0400

    Added some reference code to search.sh

--- bash/search.sh
+++ bash/search.sh
@@ -79,6 +79,93 @@ _fuzzy_find_file() {
   esac | xargs tmux respawn-pane -kt search.1
 }
 
+_explore() {
+  local usrin lastopt
+  declare -A search_categories
+  declare -a optslist
+  optslist=('Select one of the following categories:')
+  for o in "${!search_categories[@]}"; do
+    if [ -d "${search_categories[$o]}" ] &&
+      [ -n "$(ls -A ${search_categories[$o]})" ]
+      then
+      optslist+=("$o")
+    else
+      unset search_categories["$o"]
+    fi
+  done
+  if [ ${#search_categories[@]} -eq 0 ]; then
+    _find_and_display_file && return 0 || return 1
+  fi
+  optslist+=('Current Working Directory')
+  optslist+=('Quit')
+  clear
+  _display_menu "${optslist[@]}"
+  lastopt=$((${#optslist[@]}-1))
+  read -p "Enter Choice [1-$lastopt] " usrin
+  case "$usrin" in
+    $lastopt|[Qq]*)
+      return 0
+      ;;
+    $(($lastopt-1)))
+      _find_and_display_file && { _explore; return 0; } || return 1
+      ;;
+    *)
+      if ! _int_test "$usrin" || [ -z "${optslist[$usrin]}" ]; then
+        _print_error "$usrin $NOT_RECOGNIZED_OPTION" || return 2
+      else
+        pushd "${search_categories[${optslist[$usrin]}]}"
+        _find_and_display_file || return 1
+        popd
+      fi
+      ;;
+  esac
+  _explore
+}
+
+_find_and_display_file() {
+  local path
+  path=$(find * -maxdepth 4 -not -path '*/.?*/*' | fzf) || return 0
+  if [ -d "$path" ]; then
+    pushd "$path" >/dev/null
+    _find_and_display_file
+    popd >/dev/null
+  else
+    _open_file "$path"
+    _find_and_display_file
+  fi
+}
+
+_open_file() {
+  local file
+  file="${1:?}"
+  case "$file" in
+    *.log)
+      if grep -q 'The SAS System' $file; then
+        less -S -p ^ERROR: $file
+      else
+        less -S $file
+      fi
+      ;;
+    *.sas|*.sh)
+      vim -b $file
+      ;;
+    *.json)
+      jq '.' $file | less -S
+      ;;
+    *.zip|*.gz)
+      less -S $file
+      ;;
+    *)
+      if file $(realpath $file) | grep -q 'text'; then
+        less -RS $file
+      else
+        _print_error "$(realpath $file) is not a text file"
+        return 1
+      fi
+      ;;
+  esac
+}
+
 ####################
 # START
 ####################
