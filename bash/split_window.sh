8cf3daed6076b7b4ebed28420883ee8833abe50f
winnbgwrrr <winslowr@gmail.com>
2025-07-04 10:04:16 -0400

    Fixed pipe error Added ability to specify directory to explore Added split_window.sh script

---
+++ bash/split_window.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+################################################################################
+# Script:   split_window.sh                                                    #
+# Function:                                                                    #
+# Usage:    split_window.sh [-h]                                               #
+#                                                                              #
+# Author: Robert Winslow                                                       #
+# Date written: 07-04-2025                                                     #
+#                                                                              #
+################################################################################
+. $(dirname $0)/common.functions
+
+USAGE_STR='[-h]'
+
+####################
+# split_window.sh START
+####################
+help=$(cat <<END
+Program description goes here.
+
+Usage: $(basename $0) $USAGE_STR
+
+  -h            Print this help message
+
+END
+)
+
+while getopts 'h' OPT; do
+  case "$OPT" in
+    h)
+      printf '%s\n' "$help"
+      exit 0
+      ;;
+    *)
+      _usage
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ -z "$TMUX" ] && exit 0
+[ $(($(tput cols)*10/$(tput lines))) -gt 36 ] &&
+  tmux split-window -h || tmux split-window -v
+[ $# -gt 0 ] && tmux send-keys "$@" C-m
+
+exit 0
